e8b799b9ba21965c87c189a40fcc8383d1defbe3
Stan Zawieja <stanislaw.zawieja@student.nhh.no>
2020-10-09 16:37:06 +0200

    going back to normal version on master

--- demo_script.R
+++ demo_script.R
@@ -1,20 +1,10 @@
 #Headline
 
-<<<<<<< HEAD
-x <- rnorm(500)
-y <- rnorm(900)
+x <- rnorm(100)
+y <- rnorm(100)
 
 #super important feature for real it's hella important
 
 # new feature brand brand new - adding something to make feater even 
 #brander and newer
-=======
-x <- rnorm(200)
-y <- rnorm(300)
 
-#super important feature for real it's hella important
-
-# new feature brand brand new
-
-#some crazy stuff
->>>>>>> crazy-idea
