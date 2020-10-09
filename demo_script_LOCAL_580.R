f7e3307f5e4813028f15e2beaba03cdfd4036490
Stan Zawieja <stanislaw.zawieja@student.nhh.no>
2020-10-09 17:23:17 +0200

    idk what im doing

--- demo_script_LOCAL_580.R
+++
@@ -1,9 +0,0 @@
-#Headline
-
-x <- rnorm(500)
-y <- rnorm(900)
-
-#super important feature for real it's hella important
-
-# new feature brand brand new - adding something to make feater even 
-#brander and newer
